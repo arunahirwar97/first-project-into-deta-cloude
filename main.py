b0f5f3909d5ff782899b816fa2686698b5e59400
websiteaap <arun887126@gmail.com>
2022-05-10 21:19:35 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,34 @@
+from uuid import uuid4
+from traceback import format_exc
+from flask import Flask, render_template, request, redirect
+from deta import Deta
+from typing import Union
+from random import choices
+from string import ascii_lowercase, digits
+from time import time
+from os import environ
+
+app = Flask(__name__)
+deta = Deta(environ["DETA_PROJECT_KEY"])
+links = deta.Base("links")
+views = deta.Base("views")
+errors = deta.Base("errors")
+
+
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
+
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template("404.html")
+
+
+@app.errorhandler(Exception)
+def error_handler(e):
+    error = errors.put(
+        {"traceback": format_exc(), "time": int(time()), "key": str(uuid4())}
+    )
+    return render_template("error.html", error=str(e), code=error["key"])
